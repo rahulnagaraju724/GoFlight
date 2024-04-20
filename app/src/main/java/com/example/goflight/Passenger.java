f67ed103ed392d6420bdf9e73e2db2208921ccea
rahulnagaraju724 <rahulnagaraju724@gmail.com>
2024-04-20 13:06:39 -0500

    Added passenger and booking entity

---
+++ app/src/main/java/com/example/goflight/Passenger.java
@@ -0,0 +1,85 @@
+package com.example.goflight;
+
+public class Passenger {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String nationality;
+    private String dateOfBirth;
+    private String passport;
+
+    public Passenger() {
+
+    }
+
+    // Constructor
+    public Passenger(String firstName, String lastName, String phoneNumber, String nationality, String dateOfBirth, String passport) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.passport = passport;
+    }
+
+    // Getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                ", passport='" + passport + '\'' +
+                '}';
+    }
+}
