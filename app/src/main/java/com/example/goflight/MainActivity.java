b5903b76af1f551e1f31ba99f503947282d2a439
rahulnagaraju724 <rahulnagaraju724@gmail.com>
2024-04-19 20:09:22 -0500

    Some changes for home

--- app/src/main/java/com/example/goflight/MainActivity.java
+++ app/src/main/java/com/example/goflight/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static DatabaseHelper dbHelper;
     private static SQLiteDatabase db;
+
+    private EditText sourceInput, destinationInput, departureDateInput;
+    private Button searchFlightButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +23,22 @@ public class MainActivity extends AppCompatActivity {
 
         dbHelper = new DatabaseHelper(this);
         db = dbHelper.getWritableDatabase(); // This will create or open the database for writing
+
+        // Find EditText views by their IDs
+        sourceInput = findViewById(R.id.source_input);
+        destinationInput = findViewById(R.id.destination_input);
+        departureDateInput = findViewById(R.id.departure_date_input);
+        // Find Button view by its ID
+        searchFlightButton = findViewById(R.id.search_flight_button);
+
+        // Retrieve input values and store them in variables when the button is clicked
+        searchFlightButton.setOnClickListener(view -> {
+            String source = sourceInput.getText().toString();
+            String destination = destinationInput.getText().toString();
+            String departureDate = departureDateInput.getText().toString();
+            System.out.println("Source:"+source+" Destination:"+destination+" and departureDate:"+departureDate);
+        });
+
     }
 
     public static SQLiteDatabase getDatabase() {
